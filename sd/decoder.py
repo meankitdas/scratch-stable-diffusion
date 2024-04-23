ace06df99298cbd997a1e9158e546531e7ede963
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2024-04-24 01:50:07 +0530

    initial-1.0.1 ✨

---
+++ sd/decoder.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+        
+    def forward(self, x):
+        residue = x
+
+        x = self.groupnorm(x)
+        
+        
+    
+
