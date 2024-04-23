c1c5831bd833ec5a59f63ab72fb2cf970f36c428
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2024-04-24 00:33:01 +0530

    initial-1.0.1 ✨

--- sd/encoder.py
+++ sd/encoder.py
@@ -3,11 +3,60 @@ from torch import nn
 from torch.nn import functional as F
 from decoder import VAE_AttentionBlock, VAE_ResidualBlock
 
+
 class VAE_Encoder(nn.Sequential):
     def __init__(self):
         super().__init__(
             nn.Conv2d(3, 128, kernel_size=3, padding=1),
             VAE_ResidualBlock(128, 128),
             VAE_ResidualBlock(128, 128),
-            
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height / 2, Width / 2)
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(128, 256),
+            VAE_ResidualBlock(256, 256),
+            # (Batch_Size, 256, Height / 2, Width / 2) -> (Batch_Size, 256, Height / 4, Width / 4)
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(256, 512),
+            VAE_ResidualBlock(512, 512),
+            # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 8, Width / 8)
+            nn.Conv2d(512, 512, kernal_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            nn.GroupNorm(32, 512),
+            nn.SiLU(),
+            nn.Conv2d(512, 8, kernel_size=3, padding=1),
+            nn.Conv2d(8, 8, kernel_size=1, padding=0),
         )
+
+    def forward(self, x, noise):
+        # x: (Batch_Size, Channel, Height, Width)
+        # noise: (Batch_Size, 4, Height / 8, Width / 8)
+
+        for module in self:
+
+            if getattr(module, "stride", None) == (2, 2):
+                x = F.pad(x, (0, 1, 0, 1))
+                # Pad: (Padding_Left, Padding_Right, Padding_Top, Padding_Bottom).
+
+            x = module(x)
+
+        # (Batch_Size, 8, Height / 8, Width / 8) -> two tensors of shape (Batch_Size, 4, Height / 8, Width / 8)
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+
+        # Clamp the log variance between -30 and 20, so that the variance is between (circa) 1e-14 and 1e8.
+        log_variance = torch.clamp(log_variance, -30, 20)
+
+        variance = log_variance.exp()
+
+        stdv = variance.sqrt()
+
+        x = mean + stdv * noise
+        # Transform N(0, 1) -> N(mean, stdev)
+
+        # Scale by a constant
+        x *= 0.18215
+
+        return x
