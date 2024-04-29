2f73dba5657c6a058e6787ed7d3fa7314f21b12e
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2024-04-29 15:54:37 +0530

    Updated ddpm.py

--- sd/ddpm.py
+++ sd/ddpm.py
@@ -1,1 +1,26 @@
-pass
+import torch
+import numpy as np
+
+class DDPMSampler:
+    def __init__(self, generator: torch.Generator, num_training_steps = 1000, beta_start=0.00085, beta_end=0.120):
+        self.betas = torch.linspace(beta_start ** 0.5, beta_end ** 0.5, num_training_steps, dtype=torch.float32) ** 2
+        
+        self.alphas = 1 - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
+        self.one = torch.tensor(1.0)
+        
+        self.generator = generator
+        
+        self.num_train_timesteps = num_training_steps
+        self.timesteps = torch.from_numpy(np.arange(0, num_training_steps)[::-1].copy())
+        
+    def set_inference_timesteps(self, num_inference_steps=50):
+        self.num_inference_steps = num_inference_steps
+        step_ratio = self.num_train_timesteps // self.num_inference_steps
+        timesteps = (np.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64)
+        self.timesteps = torch.from_numpy(timesteps)
+        
+    def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.IntTensor) -> torch.FloatTensor:
+        alphas_cumprod = self.alphas_cumprod.to(device=original_samples.device, dtype=original_samples.dtype)
+        
+        timesteps = timesteps.to(device=original_samples.device)
